2e43b7accc57b5048a4a5aefb03cfcedad8a3e33
Leo <935987+zeropointcoder@users.noreply.github.com>
2026-04-03 15:59:35 +0100

    Add form fields

--- employee-management-system/src/app/components/employee/employee.ts
+++ employee-management-system/src/app/components/employee/employee.ts
@@ -1,5 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
 import { AsyncPipe } from '@angular/common'
 // import { HttpErrorResponse } from '@angular/common/http'
 // import { Observable, retry } from 'rxjs'
@@ -35,13 +35,24 @@ export class EmployeeComponent implements OnInit {
   
   empid: String = ''
 
-  constructor() {}
+  departments = ['Engineering', 'HR', 'Sales']
+  employmenttypes = ['Full-time', 'Part-time', 'Contract']
+
+  constructor(private cd: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.empForm = this.fb.group({
-      fullname: [''],
-      phone: [''],
-      email: ['']
+      fullname: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+
+      jobtitle: [''],
+      department: [''],
+      dateofjoining: [''],
+
+      employmenttype: [''],
+      address: [''],
+      profileimage: [''],
     })
 
     // this.onLoad()
@@ -60,16 +71,30 @@ export class EmployeeComponent implements OnInit {
   // }
 
   onAdd(): void {
-    if(this.empForm.value.fullname == '' || this.empForm.value.phone == '' || this.empForm.value.email == '') {
+    // if(this.empForm.value.fullname == '' || this.empForm.value.phone == '' || this.empForm.value.email == '') {
+    //   return
+    // }
+
+    if(this.empForm.invalid) {
       return
     }
 
-    const empObj = {
-      fullname: this.empForm.value.fullname,
-      phone: this.empForm.value.phone,
-      email: this.empForm.value.email
-    }
+    // const empObj = {
+    //   fullname: this.empForm.value.fullname,
+    //   phone: this.empForm.value.phone,
+    //   email: this.empForm.value.email,
 
+    //   jobtitle: this.empForm.value.jobtitle,
+    //   department: this.empForm.value.department,
+    //   dateofjoining: this.empForm.value.dateofjoining,
+
+    //   employmenttype: this.empForm.value.employmenttype,
+    //   address: this.empForm.value.address,
+    //   profileimage: this.empForm.value.profileimage
+    // }
+
+    const empObj = this.createEmpObj(this.empForm)
+    
     // this.es.onAdd(empObj).subscribe({
     //   next: (res: Employee) => {
     //     // this.employeeNew = res
@@ -90,15 +115,29 @@ export class EmployeeComponent implements OnInit {
   }
 
   onUpdate(): void {
-    if(this.empForm.value.fullname == '' || this.empForm.value.phone == '' || this.empForm.value.email == '') {
+    // if(this.empForm.value.fullname == '' || this.empForm.value.phone == '' || this.empForm.value.email == '') {
+    //   return
+    // }
+
+    if(this.empForm.invalid) {
       return
     }
 
-    const empObj = {
-      fullname: this.empForm.value.fullname,
-      phone: this.empForm.value.phone,
-      email: this.empForm.value.email
-    }
+    // const empObj = {
+    //   fullname: this.empForm.value.fullname,
+    //   phone: this.empForm.value.phone,
+    //   email: this.empForm.value.email,
+
+    //   jobTitle: this.empForm.value.jobtitle,
+    //   department: this.empForm.value.department,
+    //   dateofjoining: this.empForm.value.dateofjoining,
+
+    //   employmenttype: this.empForm.value.employmenttype,
+    //   address: this.empForm.value.address,
+    //   profileimage: this.empForm.value.profileimage
+    // }
+
+    const empObj = this.createEmpObj(this.empForm)
 
     // this.es.onUpdate(empObj, this.empid).subscribe({
     //   next: (res: Employee) => {
@@ -138,20 +177,73 @@ export class EmployeeComponent implements OnInit {
     this.store.dispatch(onLoad())
   }
 
+  createEmpObj(employeeForm: any): any {
+    return {
+      fullname: employeeForm.value.fullname,
+      phone: employeeForm.value.phone,
+      email: employeeForm.value.email,
+
+      jobtitle: employeeForm.value.jobtitle,
+      department: employeeForm.value.department,
+      dateofjoining: employeeForm.value.dateofjoining,
+
+      employmenttype: employeeForm.value.employmenttype,
+      address: employeeForm.value.address,
+      profileimage: employeeForm.value.profileimage
+    }
+  }
+
+  onFileChange(event: any): void {
+    const file = event.target.files[0]
+
+    if(!file) return
+
+    // validate file type
+    if(!file.type.startsWith('image/')) {
+      alert('Only image files are allowed')
+      return
+    }
+
+    // limit file size (up to 2MB)
+    if(file.size > 2 * 1024 * 1024) {
+      alert('Image must be less than 2MB')
+      return
+    }
+
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      this.empForm.patchValue({
+        profileimage: reader.result as string
+      })
+      this.cd.detectChanges(); // 🔥 stabilise
+    }
+
+    reader.readAsDataURL(file)
+  }
+
   onUpdateCta(employee: Employee): void {
     this.empForm.controls['fullname'].setValue(employee.fullname)
     this.empForm.controls['phone'].setValue(employee.phone)
     this.empForm.controls['email'].setValue(employee.email)
 
+    this.empForm.controls['jobtitle'].setValue(employee.jobtitle)
+    this.empForm.controls['department'].setValue(employee.department)
+    this.empForm.controls['dateofjoining'].setValue(employee.dateofjoining)
+
+    this.empForm.controls['employmenttype'].setValue(employee.employmenttype)
+    this.empForm.controls['address'].setValue(employee.address)
+    this.empForm.controls['profileimage'].setValue(employee.profileimage)
+
     this.empid = employee.id
   }
 
   onResetForm(): void {
-    // this.empForm.reset()
+    this.empForm.reset()
 
-    this.empForm.controls['fullname'].setValue('')
-    this.empForm.controls['phone'].setValue('')
-    this.empForm.controls['email'].setValue('')
+    // this.empForm.controls['fullname'].setValue('')
+    // this.empForm.controls['phone'].setValue('')
+    // this.empForm.controls['email'].setValue('')
 
     this.empid = ''
   }
--- employee-management-system/src/app/models/employee.model.ts
+++ employee-management-system/src/app/models/employee.model.ts
@@ -4,5 +4,13 @@ export class Employee {
     phone: String = ''
     email: String = ''
 
+    jobtitle: String = ''
+    department: String = ''
+    dateofjoining: String = ''
+    
+    employmenttype: String = ''
+    address: String = ''
+    profileimage: String = ''
+
     constructor() {}
 }
