36783a079cdb5e9e6261f6880b2f26229d54fd68
Leo <935987+zeropointcoder@users.noreply.github.com>
2026-06-14 11:51:25 +0100

    Update snippet

--- employee-access-portal/src/app/app.ts
+++ employee-access-portal/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { Header } from "./components/header/header"
-import { Footer } from "./components/footer/footer";
+import { Footer } from "./components/footer/footer"
 
 @Component({
   selector: 'app-root',
